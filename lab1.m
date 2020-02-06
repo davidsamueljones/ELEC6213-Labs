1e4bee32ebca561f423ba758f1d856c7abf39204
David Jones <davidsamueljones@icloud.com>
2020-02-06 00:32:21 +0000

    Fixed a4, added image diffs

--- lab1.m
+++ lab1.m
@@ -14,21 +14,34 @@ bmp_img = imread(bmp_path);
 %% A.2
 copy_path = fullfile(temp_dir, strcat(filename, '_2.bmp'));
 imwrite(bmp_img, copy_path);
+
 %% A.3
 jpg_path = fullfile(temp_dir, strcat(filename, '.jpg'));
 imwrite(bmp_img, jpg_path);
+
 %% A.4
-bmp_img = imread(jpg_path);
-%% A.5
+jpg_img = imread(jpg_path);
 figure()
-subplot(1, 2, 1)
+subplot(1, 3, 1)
 imshow(bmp_img)
-subplot(1, 2, 2)
-imshow(bmp_img)
+subplot(1, 3, 2)
+imshow(jpg_img)
+subplot(1, 3, 3)
+imshowpair(bmp_img, jpg_img, 'diff') 
+
+%% A.5
+midpoint = size(bmp_img) / 2;
+xs = midpoint(1) - 5:midpoint(1) + 5;
+ys = midpoint(2) - 5: midpoint(2) + 5;
+extracted = bmp_img(xs, ys);
+figure()
+imshow(extracted)
+
 %% A.6 (imcomplement)
 neg_img = intmax('uint8') - bmp_img;
 figure()
 imshow(neg_img)
+
 %% A.7
 ks = [2, 2];
 input = bmp_img;
@@ -47,22 +60,28 @@ end
 figure()
 imshow(scaled)
 %% A.8
-ks = [10, 20];
+ks = [2, 2];
 input = bmp_img;
 input_size = size(input);
 output_size = input_size;
-blurred = zeros(output_size, 'uint8');
+pixelated = zeros(output_size, 'uint8');
 for i=1:(ceil(input_size(1) / ks(1)))
     for j=1:(ceil(input_size(2) / ks(2)))
         x = (i - 1) * ks(1);
         y = (j - 1) * ks(2);
         xs = x+1:min(x+ks(1), input_size(1));
         ys = y+1:min(y+ks(2), input_size(2));
-        blurred(xs, ys) = mean(input(xs, ys), 'all');
+        pixelated(xs, ys) = mean(input(xs, ys), 'all');
     end
 end
 figure()
-imshow(blurred)
+subplot(1, 3, 1)
+imshow(bmp_img)
+subplot(1, 3, 2)
+imshow(pixelated)
+subplot(1, 3, 3)
+imshowpair(bmp_img, pixelated, 'diff') 
+
 %% A.9
 bit_cnt = 8;
 bits = cell(8);
@@ -73,5 +92,5 @@ for i=1:bit_cnt
     subplot(2, 4, i)
     imshow(bits{i})
 end
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
